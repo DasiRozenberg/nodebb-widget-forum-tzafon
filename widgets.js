cd2b0958ad1e2bd308a4a82e07ae73bc42a012b6
unknown <dasi654321@gmail.com>
2020-07-28 00:41:21 +0300

    update

--- widgets.js
+++ widgets.js
@@ -1,3 +1,21 @@
+const nconf = require.main.require('nconf');
+const validator = require.main.require('validator');
+const benchpressjs = require.main.require('benchpressjs');
+const _ = require.main.require('lodash');
+const winston = require.main.require('winston');
+
+const db = require.main.require('./src/database');
+const categories = require.main.require('./src/categories');
+const user = require.main.require('./src/user');
+const plugins = require.main.require('./src/plugins');
+const topics = require.main.require('./src/topics');
+const posts = require.main.require('./src/posts');
+const groups = require.main.require('./src/groups');
+const utils = require.main.require('./src/utils');
+const emailer = require.main.require('./src/emailer');
+const meta = require.main.require('./src/meta');
+
+
 module.exports = function(Widget) {
     let app;
 
