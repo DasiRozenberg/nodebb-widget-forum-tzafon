cff5728c24feecf9f9bd5c9db5306f8f3584369f
unknown <dasi654321@gmail.com>
2020-08-01 23:01:11 +0300

    add console.log

--- widgets.js
+++ widgets.js
@@ -109,6 +109,7 @@ module.exports = function(Widget) {
 
     function isVisibleInCategory(widget) {
         const cids = getCidsArray(widget);
+        console.log(cids, widget.templateData.template.category, widget.templateData.template.topic)
         return !(cids.length) ||
             ((widget.templateData.template.category || widget.templateData.template.topic) && cids.includes(parseInt(widget.templateData.cid, 10))) ||
             (!(widget.templateData.template.category || widget.templateData.template.topic) && cids.includes(0));
