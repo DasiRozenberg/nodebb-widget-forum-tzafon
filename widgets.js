1f26d1e38638f48bcbbcadcd85fb67001df4a212
unknown <dasi654321@gmail.com>
2020-09-10 13:10:45 +0300

    remove first image

--- widgets.js
+++ widgets.js
@@ -182,7 +182,6 @@ module.exports = function(Widget) {
         const subject = 'פורום צפון | קופונים';
         const post = await posts.getPostData(req.body.pid);
         const postData = post.content.split('\n').map(item => item !== '.' ? item : "");
-        postData[1] = nconf.get('url') + postData[1].match(/\((.*)\)/)[1];
         postData[2] = nconf.get('url') + postData[2].match(/\((.*)\)/)[1];
         const title = 'קיבלת קופון!';
         const options = {
